864b881b0f621a3bb1e444f7e894f27795ac78c6
kylewill0725 <kylewill0725@outlook.com>
2019-12-14 18:23:55 -0600

    Add project files.

---
+++ ExampleJsonProviderLibrary/Library.fs
@@ -0,0 +1,10 @@
+﻿namespace ExampleAssembly
+open FSharp.Data
+
+module Say =
+    type HelloType = JsonProvider<"./a.json", RootName = "Create", EmbeddedResource = "ExampleAssembly, a.json">
+
+    let hello () =
+        let helloType = HelloType.Create(foo = "baz")
+        printfn "Hello %s" helloType.Foo
+        helloType
